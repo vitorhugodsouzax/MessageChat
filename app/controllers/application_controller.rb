143f4a74d5dbce149ad2c96472b95bc9cbe01f33
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-07-16 14:41:16 -0300

    add sessions create and destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?      
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  
+    def logged_in?
+      !!current_user
+    end
+  
+    def require_user
+      if !logged_in?
+        flash[:alert] = "Voce precisa estar logado para fazer essa ação"
+        redirect_to login_path
+      end
+    end
 end
+
+#Mesmo sistema de login que eu usei no Blogsfera padrão
