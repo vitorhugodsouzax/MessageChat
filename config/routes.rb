bdc70359702527ee0a01d9cedd7e2e0d89f9becf
Vitor Hugo <vitorhugodesouzaph@gmail.com>
2024-07-17 15:06:51 -0300

    generate chatroom channel and add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 post 'message', to: 'messages#create'
+
+mount ActionCable.server, at: '/cable'
 end
